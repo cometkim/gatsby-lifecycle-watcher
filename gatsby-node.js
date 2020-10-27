e15868239fea7ed21e4ce3af3c31155f9335ff93
Hyeseong Kim <cometkim.kr@gmail.com>
2020-10-27 23:32:02 +0900

    log all available hooks in gatsby-node API

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const fs = require('fs');
 
-// You can delete this file if you're not using it
+const packageManifest = require('./package.json');
+const { gatsby: version } = packageManifest.dependencies
+
+const calls = {};
+
+const check = (key, cb) => {
+  if (calls[key]) return;
+  cb(key);
+  calls[key] = true;
+};
+
+const appendLog = value => {
+  fs.appendFileSync(`gatsby-${version}-lifecycle-${process.env.NODE_ENV}.log`, value + '\n', 'utf8');
+};
+
+// All hooks documented at https://www.gatsbyjs.com/docs/node-apis/
+exports.createPages = () => check('createPages', appendLog);
+exports.createPagesStatefully = () => check('createPagesStatefully', appendLog);
+exports.createResolvers = () => check('createResolvers', appendLog);
+exports.createSchemaCustomization = () => check('createSchemaCustomization', appendLog);
+exports.generateSideEffects = () => check('generateSideEffects', appendLog);
+exports.onCreateBabelConfig = () => check('onCreateBabelConfig', appendLog);
+exports.onCreateDevServer = () => check('onCreateDevServer', appendLog);
+exports.onCreateNode = () => check('onCreateNode', appendLog);
+exports.onCreatePage = () => check('onCreatePage', appendLog);
+exports.onCreateWebpackConfig = () => check('onCreateWebpackConfig', appendLog);
+exports.onPostBootstrap = () => check('onPostBootstrap', appendLog);
+exports.onPostBuild = () => check('onPostBuild', appendLog);
+exports.onPreBootstrap = () => check('onPreBootstrap', appendLog);
+exports.onPreBuild = () => check('onPreBuild', appendLog);
+exports.onPreExtractQueries = () => check('onPreExtractQueries', appendLog);
+exports.onPreInit = () => check('onPreInit', appendLog);
+exports.pluginOptionsSchema = () => check('pluginOptionsSchema', appendLog);
+exports.preprocessSource = () => check('preprocessSource', appendLog);
+exports.resolvableExtensions = () => check('resolvableExtensions', appendLog);
+exports.setFieldsOnGraphQLNodeType = () => check('setFieldsOnGraphQLNodeType', appendLog);
+exports.sourceNodes = () => check('sourceNodes', appendLog);
+exports.unstable_shouldOnCreateNode = () => check('unstable_shouldOnCreateNode', appendLog);
